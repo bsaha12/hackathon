b6d6e3bbc9187ded88899ac1279331cd2aace05d
SourabhBurman <souravburman55@gmail.com>
2024-03-04 13:41:21 +0530

    card added

--- frontend/notesapp/src/components/NotesGrid.tsx
+++ frontend/notesapp/src/components/NotesGrid.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import Masonry from 'masonry-layout';
+import { FaRegFileAlt } from 'react-icons/fa'
+import { GiClick } from "react-icons/gi";
 
 interface Note {
   text: string;
@@ -35,12 +37,29 @@ const NotesGrid: React.FC<NotesGridProps> = ({ notes, onNoteDelete }) => {
   }, [notes]);
 
   return (
-    <div className="notes-grid" ref={gridRef}>
+    <div className="notes-grid grid grid-cols-5 w-full" ref={gridRef}>
       {notes.map(note => (
-        <div key={note.id} className="note" style={{ backgroundColor: note.color }}>
-          <span className="delete-note" onClick={() => onNoteDelete(note)}> × </span>
-          {note.text}
-        </div>
+
+<div className='relative flex-shrink-0 w-44 h-60 rounded-[30px] bg-zinc-100 text-zinc-900 overflow-hidden p-6'>
+<FaRegFileAlt />
+<p className='mt-5 text-sm'>Sentiment : Positive</p>
+<div className='footer absolute bottom-0 w-full h-12 left-0 py-3 px-8 hover:bg-violet-600'>
+    <div className='flex justify-between items-center'>
+        <h5 className=''>Summary</h5>
+        <GiClick />
+    </div>
+</div>
+</div>
+
+
+
+
+
+
+        // <div key={note.id} className="note" style={{ backgroundColor: note.color }}>
+        //   <span className="delete-note" onClick={() => onNoteDelete(note)}> × </span>
+        //   {note.text}
+        // </div>
       ))}
     </div>
   );
